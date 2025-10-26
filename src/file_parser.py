761dbdc1ca840245616e99f9094087bfcfc3dfb8
Ethan Sturek <ethansturek@gmail.com>
2025-10-26 13:55:41 -0700

    File_parser now returns a list of dictionary entries with a summary of the file contents. Tests for get_input_file_path and check_file_validity have been added.

--- src/file_parser.py
+++ src/file_parser.py
@@ -1,6 +1,8 @@
 import os
 import zipfile
 
+
+# Checks user input and passes it to a validity check. This will loop if not input is entered
 def get_input_file_path():
 
     while True:
@@ -12,12 +14,27 @@ def get_input_file_path():
             print("No path was entered.")
         elif zip_path:
             # Validate path and folder before moving on
-            if check_file_validity(zip_path):
+            if (file_tree := check_file_validity(zip_path)):
                 print("Valid zip file detected.")
+                for f in file_tree:
+                    print(f["filename"], f["size"])
                 return zip_path
+            else: print("Invalid zip file detected. Please enter a valid zip file.")
 
 
 #TODO: add errors to log file 
+"""
+Validates the following in order:
+
+1. Path exists
+2. File exists
+3. Ends in .zip
+4. Corrupted file / ZIP64 / Other Errors
+5. Empty zip file
+
+For every file checked in the zip, a list of dictionary entries is made, summarizing the contents. 
+
+"""
 def check_file_validity(zip_path):
 
     if os.path.exists(zip_path):
@@ -27,28 +44,44 @@ def check_file_validity(zip_path):
                 try:
                   with zipfile.ZipFile(zip_path, 'r') as zip_ref:
                       bad = zip_ref.testzip()
-                      if bad is None:
-                          return True
-                      else:
+                      if bad:
                           print("Corrupted archive: bad entry at: ", bad)
-                          return False
+                          return None
+                      else:
+                        file_tree = [
+                            {
+                                "filename": info.filename,
+                                "size": info.file_size,
+                                "date_time": info.date_time
+                            }
+                            for info in zip_ref.infolist()
+                        ]
+                        if not file_tree:
+                            print("Zip file is valid, but empty.")
+                            return None
+
+
+                        return file_tree
+
+
+
                 except zipfile.BadZipFile:
                     print("Not a zip file or corrupted at central directory.")
-                    return False
+                    return None
                 except zipfile.LargeZipFile:
-                    print("File uses ZIP64. Cannot handle.")
-                    return False
+                    print("File uses ZIP64. Too large cannot handle.")
+                    return None
                 except Exception as e:
                     print("Error: ", e)
-                    return False
+                    return None
             else:
                 print("The requested file is not a zip file")
-                return False
+                return None
 
         else:
             print("File does not exist.")
-            return False
+            return None
                     
     else:
         print("Path does not exist")
-        return False
+        return None
