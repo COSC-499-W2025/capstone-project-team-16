103ff681bc5f2688c2b5d8776803ef2bfbf57ae3
Ethan Sturek <ethansturek@gmail.com>
2025-11-02 19:34:26 -0800

    Added tests for metadata_extractor. Added more error handling for missing JSON

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 ### This will act as our orchestrator for coordinating scan tasks
 from permission_manager import get_user_consent
 from file_parser import get_input_file_path
+from metadata_extractor import base_extraction
 
 print("Welcome to Skill Scope!")
 print("~~~~~~~~~~~~~~~~~~~~~~~")
@@ -13,7 +14,8 @@ else:
 if file_list:
     #call metadata extractor and pass file_path
     #should get a list of files with accompanying metadata.
-    print("Extracting")
+    scraped_data = base_extraction(file_list)
+    
 
 
 
